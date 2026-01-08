bcdfe8cbba7b75ca99cd2c35b9fcb6350fca3d97
oneroyalace <oneroyalace@gmail.com>
2026-01-08 12:56:59 -0500

    Don't trigger claude when @code4policy-bot message contains 'ping'

--- triggers/claude_mention_trigger.ts
+++ triggers/claude_mention_trigger.ts
@@ -17,6 +17,12 @@ const ClaudeMentionTrigger: Trigger<typeof ClaudeAssistantWorkflow.definition> =
   event: {
     event_type: TriggerEventTypes.AppMentioned,
     all_resources: true,
+    filter: {
+      version: 1,
+      root: {
+        statement: "{{data.text}} NOT CONTAINS ping",
+      },
+    },
   },
   inputs: {
     channel: {
